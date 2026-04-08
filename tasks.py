fe73c4648e3f1a4903349e96974124b815542cb6
Vetha sri <vethababu2006@gmail.com>
2026-04-08 22:12:15 +0530

    Add TASKS dictionary with difficulty levels

---
+++ tasks.py
@@ -0,0 +1,14 @@
+TASKS = {
+    "easy": {
+        "lane1": 5,
+        "lane2": 3
+    },
+    "medium": {
+        "lane1": 20,
+        "lane2": 5
+    },
+    "hard": {
+        "lane1": 15,
+        "lane2": 15
+    }
+}
